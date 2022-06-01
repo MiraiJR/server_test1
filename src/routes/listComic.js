9ad3483f98417cba0c81ba42161493bed66da74f
unknown <truongvanhao159@gmail.com>
2022-06-01 19:14:23 +0700

    fix name file

--- src/routes/listComic.js
+++ src/routes/listComic.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-const listComicController = require('../app/controllers/listComicController')
+const listComicController = require('../app/controllers/ListComicController')
 
 router.get('/', listComicController.showList)
 
