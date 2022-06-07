b1f651d78343c486d113ac92f91f399e0c772745
unknown <truongvanhao159@gmail.com>
2022-06-07 22:10:46 +0700

    fix

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -2,14 +2,15 @@ const Comic = require('../models/Comic')
 const History = require('../models/History')
 const { mutipleMongooseToObject } = require('../../util/mongoose');
 const axios = require('axios')
+const cheerio = require('cheerio')
 
 class SiteController {
     home(req, res, next) {
         Comic.find({})
             .then((comics) => {
                 axios.get('https://api.db-ip.com/v2/free/self')
-                    .then(resp => {
-                        History.findOne({ip: resp.data.ipAddress})
+                    .then(IPclient => {
+                        History.findOne({ip: IPclient.data.ipAddress})
                             .then((history) => {
                                 const listComicHistory = history.comicHistory
                                 return res.render('home', {
